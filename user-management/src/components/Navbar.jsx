fad4d5c0c963ee397c0ead6ebdb0d223ad9ecc2a
Aman Sultan Baig <amansultanbaig@gmail.com>
2024-01-16 15:26:45 +0500

    component designing

---
+++ user-management/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container">
+        <a className="navbar-brand" href="#">
+          Redux Toolkit Application
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ user-management/src/pages/users/UserList.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+export const UserList = () => {
+  return (
+    <div className="row d-flex mt-4">
+      <div className="col-md-10 fw-bold">User Managemet</div>
+      <div className="d-flex col-md-2 justify-content-end">
+        <button type="button" class="btn btn-sm btn-outline-secondary">
+          Add User
+        </button>
+      </div>
+
+      <div className="col-md-12">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
